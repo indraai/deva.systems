6ffb984bea62c5d6a8d3131b99ddd4adff924de4
Quinn <quinn@indra.ai>
2023-07-08 14:10:28 -0700

    update agent spec

--- index.js
+++ index.js
@@ -26,21 +26,13 @@ const {agent,vars} = require(data_path).DATA;
 const Deva = require('@indra.ai/deva');
 const SYSTEMS = new Deva({
   info,
-  agent: {
-    id: agent.id,
-    key: agent.key,
-    describe: agent.describe,
-    prompt: agent.prompt,
-    voice: agent.voice,
-    profile: agent.profile,
-    translate(input) {
-      return input.trim();
-    },
-    parse(input) {
-      return input.trim();
-    }
-  },
+  agent,
   vars,
+  utils: {
+    translate(input) {return input.trim();},
+    parse(input) {return input.trim();},
+    process(input) {return input.trim();},
+  },
   listeners: {},
   modules: {},
   deva: {},
@@ -56,64 +48,7 @@ const SYSTEMS = new Deva({
       systems.personal.answers.push(packet);
     },
   },
-  methods: {
-    /**************
-    method: issue
-    params: packet
-    describe: create a new issue for the main deva.world through github agent.
-    ***************/
-    issue(packet) {
-      const agent = this.agent();
-      return new Promise((resolve, reject) => {
-        this.question(`#github issue:${agent.key} ${packet.q.text}`).then(issue => {
-          return resolve({
-            text: issue.a.text,
-            html: issue.a.html,
-            data: issue.a.data,
-          })
-        }).catch(err => {
-          return this.error(err, packet, reject);
-        });
-      });
-    },
-
-    /**************
-    method: uid
-    params: packet
-    describe: Return a system id to the user from the :name:.
-    ***************/
-    uid(packet) {
-      return Promise.resolve({text:this.uid()});
-    },
-
-    /**************
-    method: status
-    params: packet
-    describe: Return the current status of the :name:.
-    ***************/
-    status(packet) {
-      return this.status();
-    },
-
-    /**************
-    method: help
-    params: packet
-    describe: The Help method returns the information on how to use the :name:.
-    ***************/
-    help(packet) {
-      return new Promise((resolve, reject) => {
-        this.lib.help(packet.q.text, __dirname).then(help => {
-          return this.question(`#feecting parse ${help}`);
-        }).then(parsed => {
-          return resolve({
-            text: parsed.a.text,
-            html: parsed.a.html,
-            data: parsed.a.data,
-          });
-        }).catch(reject);
-      });
-    }
-  },
+  methods: {},
   onDone(data) {
     this.listen('devacore:question', packet => {
       if (packet.q.text.includes(this.vars.trigger)) return this.func.sys_question(packet);
