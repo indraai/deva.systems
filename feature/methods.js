9cd6d04c813e0c0ccfa07860922bded1d0201bac
Quinn <quinn@indra.ai>
2023-07-08 20:57:59 -0700

    update systems

---
+++ feature/methods.js
@@ -0,0 +1,34 @@
+module.exports = {
+  /**************
+  method: service
+  params: packet
+  describe: The global service feature that installs with every agent
+  ***************/
+  systems(packet) {
+    this.context('feature');
+    const services = this.systems();
+    const data = {};
+    return new Promise((resolve, reject) => {
+      this.question(`#docs raw feature/systems`).then(doc => {
+        data.doc = doc.a.data;
+        const info = [
+          `## Systems`,
+          `::begin:systems:${systems.id}`,
+          `client: ${systems.client_name}`,
+          `concerns: ${systems.concerns.join(', ')}`,
+          `::end:systems:${this.hash(systems)}`,
+        ].join('\n');
+        const text = doc.a.text.replace(/::info::/g, info)
+        return this.question(`#feecting parse ${text}`)
+      }).then(feecting => {
+        return resolve({
+          text: feecting.a.text,
+          html: feecting.a.html,
+          data: systems
+        });
+      }).catch(err => {
+        return this.error(err, packet, reject);
+      })
+    });
+  },
+};
