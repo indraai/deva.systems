6eb181da76dfe9ca3e5ce165138b9c77b8f40e37
Quinn Michaels <quinn@indra.ai>
2025-04-20 14:17:59 -0700

    Update Deva Core systems

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
 
 import Deva from '@indra.ai/deva';
 import pkg from './package.json' with {type:'json'};
-
-import data from './data.json' with {type:'json'};
-const {agent,vars} = data.DATA;
+const {agent,vars} = pkg.data;
 
 // set the __dirname
 import {dirname} from 'node:path';
